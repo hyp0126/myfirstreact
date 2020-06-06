d7c72bbc80e5f6af03e0f547fc3665209a266b9d
YHong <hyp0126@gmail.com>
2020-06-06 19:43:18 -0400

    week#2 1/2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import PropTypes from "prop-types";
+
+function Food({name, rating}) {
+  return (
+    <div>
+    <h1>I love {name}</h1>
+    <h2>{rating}/5.0</h2>
+    </div>
+  );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
+const foodILike = [
+  {id: 1, name: "kimchi", rating: 4},
+  {id: 2, name: "ramen", rating: 3},
+  {id: 3, name: "noodle", rating: 1}
+];
 
 function App() {
-  return <div>Hello!!!</div>;
+  return <div>
+    {foodILike.map(dish => <Food key={dish.id} name={dish.name} rating={dish.rating} />)}
+    </div>;
 }
 
 export default App;
