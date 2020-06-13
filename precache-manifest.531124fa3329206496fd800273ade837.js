96aa7b7e5b382e50d1247abde19a47803f18d090
YHong <hyp0126@gmail.com>
2020-06-12 22:42:16 -0400

    Updates

---
+++ precache-manifest.531124fa3329206496fd800273ade837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58001761548d8c1382d0a9c04fc480d1",
+    "url": "/myfirstreact/index.html"
+  },
+  {
+    "revision": "698d369d022d76574cd5",
+    "url": "/myfirstreact/static/css/main.8a5589dd.chunk.css"
+  },
+  {
+    "revision": "50bee2acbc03f20f1e32",
+    "url": "/myfirstreact/static/js/2.398def0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myfirstreact/static/js/2.398def0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698d369d022d76574cd5",
+    "url": "/myfirstreact/static/js/main.bb8f86b0.chunk.js"
+  },
+  {
+    "revision": "3bd68e7d08e6c0632ad8",
+    "url": "/myfirstreact/static/js/runtime-main.df7a1397.js"
+  }
+]);
